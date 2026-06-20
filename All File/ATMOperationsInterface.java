0eab187a7770194452267e216de89d3a78b86c12
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-20 16:40:40 +0530

    Add files via upload

---
+++ All File/ATMOperationsInterface.java
@@ -0,0 +1,10 @@
+package all_class;
+
+public interface ATMOperationsInterface {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount);
+    public void vieewMiniStatement();
+
+    
+}
